0d59efd49f319ba4458e5e58778ea0893716e84b
tomasaguileracastillo <tomasaguilerac@live.cl>
2024-12-11 23:31:52 -0300

    creacion de codigo inicial

---
+++ core/core/src/test/java/prueba/tienda/core/MicroservicioSpringBootCoreDeTiendaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.tienda.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSpringBootCoreDeTiendaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
